f613659b7480935e667aed9f54b0ad714f31c616
brightside67 <mobrika@list.ru>
2020-03-23 13:16:30 +0300

    pm2 fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,11 @@ module.exports = {
       name: "sortwebapp",
       script: "./index.js",
       watch: true,
+      increment_var: "PORT",
       env: {
         PORT: 80,
         instance_var: "INSTANCE_ID",
         NODE_ENV: "development"
-      },
-      env_production: {
-        PORT: 3000,
-        instance_var: "INSTANCE_ID",
-        NODE_ENV: "production"
       }
     }
   ]
