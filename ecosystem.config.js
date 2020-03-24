bac3af6fb57174d948773d29c48222299137ccb0
brightside67 <mobrika@list.ru>
2020-03-24 10:51:13 +0300

    add ignore files to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "./index.js",
       watch: true,
       increment_var: "PORT",
+      ignore_watch: ["node_modules", "logfile.log", "./logfile.log"],
       env: {
         PORT: 80,
         instance_var: "INSTANCE_ID",
